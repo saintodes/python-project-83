385cf1e96cef41f17d06e971e5cf8bbe3d878f16
yoga_vmu <odes36@gmailcom>
2023-10-11 23:58:36 +0700

    CDN Boostrap connection

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return "Hello World Wide Web!"
+    return render_template('index.html')
